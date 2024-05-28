3aad98886e1bb711172362b9107811908e40deb6
nosetr <osetrov.n@gmail.com>
2024-05-28 23:15:31 +0200

    first commit

---
+++ backend/src/main/java/com/nosetr/video/hub/FirebaseVideoHubApplication.java
@@ -0,0 +1,13 @@
+package com.nosetr.video.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseVideoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseVideoHubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nosetr/video/hub/FirebaseVideoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosetr.video.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseVideoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
